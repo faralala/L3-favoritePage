9546660e59e4ec9e6d12b71d55a228c220c815cb
Mikhail Cherkasov <faralala207@gmail.com>
2023-10-20 14:15:13 +0300

    First commit

---
+++ src/modules/productDetail/productDetail.ts
@@ -0,0 +1,92 @@
+import { Component } from '../component';
+import { ProductList } from '../productList/productList';
+import { formatPrice } from '../../utils/helpers';
+import { ProductData } from 'types';
+import html from './productDetail.tpl.html';
+import { cartService } from '../../services/cart.service';
+import { favoriteService } from '../../services/favorite.service';
+
+class ProductDetail extends Component {
+  more: ProductList;
+  product?: ProductData;
+
+  constructor(props: any) {
+    super(props);
+
+    this.more = new ProductList();
+    this.more.attach(this.view.more);
+  }
+
+  async render() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = Number(urlParams.get('id'));
+
+    const productResp = await fetch(`/api/getProduct?id=${productId}`);
+    this.product = await productResp.json();
+
+    if (!this.product) return;
+
+    const { id, src, name, description, salePriceU } = this.product;
+
+    this.view.photo.setAttribute('src', src);
+    this.view.title.innerText = name;
+    this.view.description.innerText = description;
+    this.view.price.innerText = formatPrice(salePriceU);
+    this.view.btnBuy.onclick = this._addToCart.bind(this);
+    this.view.btnFav.onclick = this._addAndDelToFavorites.bind(this);
+
+    const isInCart = await cartService.isInCart(this.product);
+    const isInFav = await favoriteService.check(this.product);
+
+    if (isInCart) this._setInCart();
+    if (isInFav) this._setInFavList();
+    if (!isInFav) this._setNotInFavList();
+
+    fetch(`/api/getProductSecretKey?id=${id}`)
+      .then((res) => res.json())
+      .then((secretKey) => {
+        this.view.secretKey.setAttribute('content', secretKey);
+      });
+
+    fetch('/api/getPopularProducts')
+      .then((res) => res.json())
+      .then((products) => {
+        this.more.update(products);
+      });
+  }
+
+  private _addToCart() {
+    if (!this.product) return;
+
+    cartService.addProduct(this.product);
+    this._setInCart();
+  }
+
+  private _setInCart() {
+    this.view.btnBuy.innerText = '✓ В корзине';
+    this.view.btnBuy.disabled = true;
+  }
+
+  private async _addAndDelToFavorites() {
+    if (!this.product) return;
+
+    const isInFav = await favoriteService.check(this.product);
+
+    if (isInFav) {
+      favoriteService.remove(this.product);
+      this._setNotInFavList();
+    } else {
+      favoriteService.add(this.product);
+      this._setInFavList();
+    }
+  }
+
+  private _setInFavList() {
+    this.view.btnFav.title = 'В избранном';
+  }
+  private _setNotInFavList() {
+    this.view.btnFav.title = 'Добавить в избранное';
+  }
+}
+
+export const productDetailComp = new ProductDetail(html);
